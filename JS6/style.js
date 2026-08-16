c6d1eb4b00d97698f19c01808e1990f2f1220e9e
KRISHNA DEVANSH RAO <100765720+Krishna-Devansh-Rao@users.noreply.github.com>
2026-08-16 13:42:01 +0530

    Add files via upload

---
+++ JS6/style.js
@@ -0,0 +1,53 @@
+// function name(){
+//     console.log("Krishna Devansh Rao");
+// }
+
+// name() ;
+
+// function loop(){
+//     let n =  6; 
+//     for(let i=0;i <n;i++){
+//         console.log(i)
+//     }
+// }
+
+// loop();
+
+//Dice;
+
+// function rolldice(){
+//     let rand = Math.floor(Math.random() * 6 ) + 1 ;
+//     console.log(rand);
+// }
+
+// rolldice();
+
+
+// Function with args
+
+// function info(name,age){
+//     console.log(`${name}'s age is ${age}.`);
+// }
+
+// info("Krishna",19);
+// info("Nishank",19)
+
+
+
+const calculator = {
+    add: function(a,b){
+        return a+b;
+    }, 
+    sub: function(a,b){
+        return a-b;
+    } ,
+    mul: function(a,b){
+        return a*b;
+    } ,
+    Divide: function(a,b){
+        return a/b;
+    } 
+
+    };
+
+
---
+++ JS7/text.js
@@ -0,0 +1,45 @@
+// const student = {
+//     name: "Krishna",
+//     course: "BCA",
+//     phy: 92,
+//     chm: 94,
+//     maths: 98,
+
+//     getAvg(){
+//         let avg = (this.phy + this.chm + this.maths) / 3 ;
+//         console.log(avg); 
+
+//     }
+// };
+
+// //Arrow function
+// const sum = (a,b) =>{
+//     console.log(a+b);
+// }
+// sum(5,9);
+
+//impicit return
+// const multiply = (a,b) => a * b ;
+// multiply(5,9);
+
+//Set Timeout
+
+    // console.log("What's up");
+    // setTimeout(() => {
+    //     console.log("Yo Krishna keep going");
+        
+    // },3000);
+    
+    // console.log("Bro");
+    
+    
+//Set interval
+console.log("What's up");
+
+// setInterval(() => {
+//     console.log("Keep going")
+// },3000 );
+
+
+console.log("Bro");
+
